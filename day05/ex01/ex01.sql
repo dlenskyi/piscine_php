f8bddb0e6d67311c2f9d2d6ccd7e46782334dc01
Denys LENSKYI <dlenskyi@e1r2p9.unit.ua>
2019-01-22 13:27:43 +0200

    fixed d05

--- day05/ex01/ex01.sql
+++ day05/ex01/ex01.sql
@@ -1,5 +1,5 @@
-CREATE TABLE IF NOT EXISTS ft_table (
+CREATE TABLE ft_table (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    login VARCHAR(8) NOT NULL DEFAULT 'toto',
+    login VARCHAR(8) DEFAULT 'toto' NOT NULL,
     `group` ENUM('staff', 'student', 'other') NOT NULL,
     creation_date DATE NOT NULL);
